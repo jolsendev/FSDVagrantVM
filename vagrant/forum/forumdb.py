76772a619c3dff6a18e15811bf5dfc3c7c267bb5
jolsendev <jolsendev@gmail.com>
2017-08-16 15:42:07 -0700

    sql injection fix

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -7,29 +7,18 @@ import psycopg2 as psycopg2
 
 def get_posts():
     """Return all posts from the 'database', most recent first."""
-    posts = [("This is the first post.", datetime.datetime.now())]
     conn = psycopg2.connect("dbname=forum")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM posts;")
-    all_posts = cur.fetchall()
-
-    for post in all_posts:
-        content = post[0]
-        time = post[1]
-        post_tuple = (content, time)
-        posts.append(post_tuple)
-
-    conn.commit()
-    cur.close()
+    cur.execute("SELECT content, time FROM posts order by time desc;")
+    posts = cur.fetchall()
     conn.close()
-    return reversed(posts)
+    return posts
 
 
 def add_post(content):
     """Add a post to the 'database' with the current timestamp."""
     conn = psycopg2.connect("dbname=forum")
     cur = conn.cursor()
-    cur.execute('INSERT INTO posts (content, time) VALUES (%s, %s)', (content, datetime.datetime.now()))
+    cur.execute("INSERT INTO posts (content) VALUES (%s)", (content,))
     conn.commit()
-    cur.close()
     conn.close()
